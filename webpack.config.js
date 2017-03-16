34e38fb4ef44d8655544da9958c0572b88f2e14e
Ben Zulauf <bezulauf@microsoft.com>
2017-03-16 16:04:09 -0700

    express

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,48 @@
+"use strict";
+
+const debug = process.env.NODE_ENV !== "production";
+
 const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-    entry: path.join(__dirname, 'src', 'app-client.js'),
-    output: {
-        path: path.join(__dirname, 'src', 'static', 'js'),
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [{
-            test: path.join(__dirname, 'src'),
-            loader: ['babel-loader'],
-            query: {
-                cacheDirectory: 'babel-cache',
-                presets: ['react', 'es2015']
-            }
-        }]
-    },
-    debug: true,
-    devtool: '#eval-source-map',
-    plugins: [
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-        }),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurrenceOrderPlugin(),
-        new webpack.optimize.UglifyJsPlugin({
-            compress: { warnings: false },
-            mangle: false,
-            sourcemap: true,
-            beautify: false,
-            dead_code: true
-        })
-    ]
+  devtool: debug ? 'inline-sourcemap' : null,
+  entry: path.join(__dirname, 'src', 'app-client.js'),
+  devServer: {
+    inline: true,
+    port: 3333,
+    contentBase: "src/static/",
+    historyApiFallback: {
+      index: '/index-static.html'
+    }
+  },
+  output: {
+    path: path.join(__dirname, 'src', 'static', 'js'),
+    publicPath: "/js/",
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: path.join(__dirname, 'src'),
+      loader: ['babel-loader'],
+      query: {
+        cacheDirectory: 'babel_cache',
+        presets: debug ? ['react', 'es2015', 'react-hmre'] : ['react', 'es2015']
+      }
+    }]
+  },
+  plugins: debug ? [] : [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: { warnings: false },
+      mangle: true,
+      sourcemap: false,
+      beautify: false,
+      dead_code: true
+    }),
+  ]
 };
